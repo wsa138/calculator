7795c1fe02d481287e4522152bdc6671a5f90af4
William Addesso <wsaddesso@gmail.com>
2020-11-11 03:25:53 -0500

    Fixed issue with calculator where floats were converted into intergers. Added and fixed up html/css to display the current operator. Made fonts larger and corrected button sized.

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ var operatorButton = document.getElementsByClassName("operator");
 
 var equalsButton = document.getElementById("equals");
 
+var operatorDisplay = document.getElementById("operatorDisplay");
+
+// Array that holes all numbers and operators entered into calculator.
+var calcArray = [];
+
 
 // Adds event listener for each individual number button click and populates display.
 for (i=0; i<10; i++) {
@@ -24,9 +29,45 @@ clearButton.addEventListener("click", resetCalc);
 // Adds event listener to backspace button and removes number from display.
 backspaceButton.addEventListener("click", backspace);
 
+// When operator button is clicked, adds the number and the operator to the calc array.
+for (i = 0; i < operatorButton.length; i++) {
+    operatorButton[i].addEventListener("click", function() {
+        calcArray.push(currentDisplay.textContent);
+        calcArray.push(this.textContent);
+        clearDisplay();
+        let calcLength = calcArray.length;
+        if (calcArray[calcLength - 1] === this.textContent) {
+            operatorDisplay.textContent = this.textContent;
+        }
+    })
+}
+
+// Event listener on equals button provides the answer to answer equation.
+equalsButton.addEventListener("click", function() {
+    calcArray.push(currentDisplay.textContent);
+    let operator = "";
+    let answer = "";
+    for (i = 0; i < calcArray.length; i++) {
+        if (calcArray[i] === "+" || calcArray[i] === "-" || calcArray[i] === "/" || calcArray[i] === "x") {
+            operator = calcArray[i];
+        } else {
+            if (answer == "") {
+                answer = calcArray[i];
+            } else {
+                answer = operate(answer, calcArray[i], operator)
+            }
+        }
+    }
+    currentDisplay.textContent = answer;
+    operatorDisplay.textContent = "=";
+    calcArray = [];
+})
 
 
-function addition(firstNumber, secondNumber) {
+
+
+
+function addition(firstNumber, secondNumber) {;
     return (firstNumber + secondNumber);
 }
 
@@ -43,10 +84,10 @@ function division(firstNumber, secondNumber) {
 }
 
 function operate(firstNumber, secondNumber, operator) {
-    let first = parseInt(firstNumber);
-    let second = parseInt(secondNumber);
+    let first = parseFloat(firstNumber);
+    let second = parseFloat(secondNumber);
     switch (operator) {
-        case "+" :
+        case "+" :;
             return addition(first, second);
         case "-" :
             return subtraction(first, second);
@@ -79,43 +120,6 @@ function backspace() {
     let newDisplay = displayCopy.slice(0, (displayCopy.length - 1));
     currentDisplay.innerHTML = newDisplay;
 }
-
-
-
-// Array that holes all numbers and operators entered into calculator.
-var calcArray = [];
-
-for (i = 0; i < operatorButton.length; i++) {
-    operatorButton[i].addEventListener("click", function() {
-        calcArray.push(currentDisplay.textContent);
-        calcArray.push(this.textContent);
-        clearDisplay();
-    })
-}
-
-equalsButton.addEventListener("click", function() {
-    calcArray.push(currentDisplay.textContent);
-    let operator = "";
-    let answer = "";
-    for (i = 0; i < calcArray.length; i++) {
-        if(calcArray[i] === "+" || calcArray[i] === "-" || calcArray[i] === "/" ||calcArray[i] === "x") {
-            operator = calcArray[i];
-        } else {
-            if (answer === "") {
-                answer = calcArray[i];
-            } else {
-                answer = operate(parseInt(answer), parseInt(calcArray[i]), operator)
-            }
-        }
-    }
-    currentDisplay.textContent = answer;
-    calcArray = [];
-})
-
-
-
-
-
 
 function resetCalc() {
     oldNUmber = 0;
